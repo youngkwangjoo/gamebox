e5b40c10c1eb6ba7ec204e51859c959066b5999f
youngkwangjoo <dudrknd1642@gmail.com>
2024-11-29 02:53:56 +0900

    feat: game 초기 제작

--- gamebox/settings.py
+++ gamebox/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'liargame',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'gamebox.wsgi.application'
-
+ASGI_APPLICATION = 'gamebox.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- liargame/views.py
+++ liargame/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def chat(request):
+    return render(request, 'chat.html')
