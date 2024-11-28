2052a67605ef61c7ae07532e299f9a1be40a34ee
youngkwangjoo <dudrknd1642@gmail.com>
2024-11-29 04:19:53 +0900

    feat: django template 추가

--- liargame/views.py
+++ liargame/views.py
@@ -1,9 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'home.html')
 
-def index(request):
-    return HttpResponse("Welcome to the Liar Game!")
-
-def chat(request):
-    return render(request, 'chat.html')
+def join(request):
+    return render(request, 'join.html')
