c25cb72dddfe16c86b637ad25f3168211c1e795a
youngkwangjoo <dudrknd1642@gmail.com>
2025-01-12 12:35:04 +0900

    fix: asgi file 초기화코드

--- gamebox/asgi.py
+++ gamebox/asgi.py
@@ -1,10 +1,12 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from liargame.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamebox.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
